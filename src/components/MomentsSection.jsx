06dd6f2ecad5fbf93d9838a7a9076088a812b92c
galibh1 <galibh233@gmail.com>
2026-03-20 13:53:39 +0600

    change

--- src/components/MomentsSection.jsx
+++ src/components/MomentsSection.jsx
@@ -9,6 +9,7 @@ const moments = [
   { src: "/images/moment-6.jpg", className: "md:col-span-1 md:row-span-2" }, // bottom middle
   // Optional (if you downloaded it)
  
+  
 ];
 
 function Tile({ src }) {
